9c596aa0d87271cfc4d3c68e007867ebb808858a
AliciaPak25 <aliciappak@gmail.com>
2022-04-11 14:03:14 -0300

    packages of react navigation installed

---
+++ src/components/Drawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from '../screens/homeScreen';
+/* import CitiesScreen from '../screens/CitiesScreen';
+import Tabs from './Tabs'; */
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigator() {
+    return (
+        <Drawer.Navigator initialRouteName='Home'>
+            <Drawer.Screen name='Home' component={HomeScreen} />
+         {/*    <Drawer.Screen name='Cities' component={CitiesScreen} />
+            <Drawer.Screen name='User' component={Tabs} /> */}
+        </Drawer.Navigator>
+    );
+}
---
+++ src/styles/HomeStyles.js
@@ -0,0 +1,6 @@
+import {StyleSheet} from 'react-native';
+
+export const HomeStyles = StyleSheet.create({ 
+    
+
+})
